d55382cfa38574139a345ce5ba16bff32b4235de
Jan Švábík <jansvabik@jansvabik.cz>
2019-03-16 22:04:07 +0100

    Added functionality for creating native window etc. using Electron

---
+++ src/app.js
@@ -0,0 +1,44 @@
+/**
+ * THE CALCULATOR
+ * 
+ * @file app.js
+ * @brief Entry point and Electron settings
+ * @author Jan Svabik (xsvabi00)
+ * @version 1.0
+ */
+
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// init main window
+let mainWindow;
+
+const createWindow = () => {
+    // create a new instance of window and set it up
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: __dirname + '/img/icon.png',
+    });
+
+    // display calc.html page as the calculator GUI
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'calc.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+
+    // reset mainWindow when the window is closed
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+};
+
+// create the main window when app is ready
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform === 'darwin')
+        app.quit();
+});
